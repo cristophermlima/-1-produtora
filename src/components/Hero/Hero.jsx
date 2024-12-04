51aebef52d8e428d223e3a57375b76780efaf0b0
Cristopher <matheuslima0251@gmail.com>
2024-12-04 09:35:17 -0300

    att video header

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,5 +1,5 @@
 import { Link } from 'react-router-dom';
-import React, { useState, useEffect } from 'react';
+import  { useState, useEffect } from 'react';
 import './Hero.css';
 import Button from '../Button/Button';
 import Foto from '../../assets/foto.jpeg';
